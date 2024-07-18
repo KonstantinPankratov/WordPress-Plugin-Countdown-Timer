e833fcee0c5d097f7decd630154e2d1b45e1c16d
Konstantin Pankratov PC <pankratovkm@gmail.com>
2024-07-18 17:41:48 +0200

    Bring back for npm run dev

---
+++ react/src/main.tsx
@@ -0,0 +1,24 @@
+import { createRoot } from 'react-dom/client';
+import CountdownTimer from './timer/CountdownTimer';
+import CountdownTimerEditor from './wordpress/CountdownTimerEditor';
+
+document.addEventListener('DOMContentLoaded', function () {
+    const containers = document.querySelectorAll('.the-countdown-timer-component');
+
+    containers.forEach(container => {
+        const root = createRoot(container);
+        const config = container.getAttribute('data-config');
+        if (config !== null) {
+            root.render(<CountdownTimer config={JSON.parse(config)}/>);
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const containers = document.querySelectorAll('.the-countdown-timer-editor-component');
+
+    containers.forEach(container => {
+        const root = createRoot(container);
+        root.render(<CountdownTimerEditor />);
+    });
+});
