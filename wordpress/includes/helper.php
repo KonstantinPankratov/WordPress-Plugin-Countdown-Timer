a54305332ea11bd98d5ae19906d0697d6682b4a1
KonstantinPankratov <hello@kopa.pw>
2025-03-13 20:57:36 +0100

    Remove wp_register_script_module as does not suppot wp_localize_script

--- wordpress/includes/helper.php
+++ wordpress/includes/helper.php
@@ -37,20 +37,14 @@ function the_cdt_enqueue_bundle($manifest, $bundle, $depth = 0)
         return;
     }
 
-    if (function_exists('wp_register_script_module')) { // wp_register_script_module & wp_enqueue_script_module (WP6.5+)
-        wp_register_script_module($scriptTag, $scriptPath, ['wp-element'], $scriptVersion, true);
-        wp_enqueue_script_module($scriptTag);
-    } else {
-        wp_register_script($scriptTag, $scriptPath, ['wp-element'], $scriptVersion, true);
-        wp_enqueue_script($scriptTag);
-        add_filter('script_loader_tag', function ($tag, $handle, $src) use ($scriptTag) {
-            if ($handle === $scriptTag) {
-                return str_replace('<script ', '<script type="module" ', $tag);
-            }
-            return $tag;
-        }, 10, 3);
-    }
-
+    wp_register_script($scriptTag, $scriptPath, ['wp-element'], $scriptVersion, true);
+    wp_enqueue_script($scriptTag);
+    add_filter('script_loader_tag', function ($tag, $handle, $src) use ($scriptTag) {
+        if ($handle === $scriptTag) {
+            return str_replace('<script ', '<script type="module" ', $tag);
+        }
+        return $tag;
+    }, 10, 3);
     foreach ($cssFiles as $cssFile) {
         $cssPath = THE_CDT_PLUGIN_COMPONENTS_BUILD_URL . '/' . $cssFile;
         $cssVersion = file_exists($cssPath) ? filemtime($cssPath) : null;
