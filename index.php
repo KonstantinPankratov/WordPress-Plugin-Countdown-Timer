df236f17ca0bd74bee9d487ba1602131314c15ad
KonstantinPankratov <hello@kopa.pw>
2025-03-13 16:45:25 +0100

    Fix description

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 
 /*
-Plugin Name: Countdown timer
+Plugin Name: The Countdown Timer
 Description: Add countdown timer to your website
 Version: 1.0.0
 Author: Konstantin Pankratov
 Author URI: http://kopa.pw/
+License: GPLv2 or later
 */
-
 if (!defined('ABSPATH')) exit;
 
 require_once plugin_dir_path(__FILE__) . '/wordpress/init.php';
