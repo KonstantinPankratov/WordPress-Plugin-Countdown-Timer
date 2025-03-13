a9caeb6462b14e61de36740a4e381ba43579848f
KonstantinPankratov <hello@kopa.pw>
2025-03-13 16:21:49 +0100

    Fix: move the_cdt_enqueue_bundle to global scope to avoid redeclaring when placing more than one shortcode

--- wordpress/includes/helper.php
+++ wordpress/includes/helper.php
@@ -10,57 +10,6 @@ function the_cdt_load_scripts($type = 'default')
         $bundleKey = 'src/the-cdt-editor.tsx';
     }
 
-    function the_cdt_enqueue_bundle($manifest, $bundle, $depth = 0)
-    {
-        if ($depth > 3) {
-            return;
-        }
-
-        $jsFile = $bundle['file'];
-        $cssFiles = isset($bundle['css']) ? $bundle['css'] : [];
-        $scriptTag = $depth === 0 ? 'the-countdown-timer' : basename($jsFile);
-        $scriptPath = THE_CDT_PLUGIN_COMPONENTS_BUILD_URL . '/' . $jsFile;
-        $scriptVersion = file_exists($scriptPath) ? filemtime($scriptPath) : null;
-
-        if (wp_script_is($scriptTag, 'enqueued')) {
-            return;
-        }
-
-        if (function_exists('wp_register_script_module')) { // wp_register_script_module & wp_enqueue_script_module (WP6.5+)
-            wp_register_script_module($scriptTag, $scriptPath, ['wp-element'], $scriptVersion, true);
-            wp_enqueue_script_module($scriptTag);
-        } else {
-            wp_register_script($scriptTag, $scriptPath, ['wp-element'], $scriptVersion, true);
-            wp_enqueue_script($scriptTag);
-            add_filter('script_loader_tag', function ($tag, $handle, $src) use ($scriptTag) {
-                if ($handle === $scriptTag) {
-                    return str_replace('<script ', '<script type="module" ', $tag);
-                }
-                return $tag;
-            }, 10, 3);
-        }
-
-        foreach ($cssFiles as $cssFile) {
-            $cssPath = THE_CDT_PLUGIN_COMPONENTS_BUILD_URL . '/' . $cssFile;
-            $cssVersion = file_exists($cssPath) ? filemtime($cssPath) : null;
-
-            wp_enqueue_style(
-                basename($cssFile),
-                $cssPath,
-                [],
-                $cssVersion
-            );
-        }
-
-        if (isset($bundle['imports'])) {
-            foreach ($bundle['imports'] as $import) {
-                if (isset($manifest[$import])) {
-                    the_cdt_enqueue_bundle($manifest, $manifest[$import], ++$depth);
-                }
-            }
-        }
-    }
-
     if (file_exists($manifest_path)) {
         $manifest = json_decode(file_get_contents($manifest_path), true);
 
@@ -69,5 +18,56 @@ function the_cdt_load_scripts($type = 'default')
         }
 
         the_cdt_enqueue_bundle($manifest, $manifest[$bundleKey]);
+    }
+}
+
+function the_cdt_enqueue_bundle($manifest, $bundle, $depth = 0)
+{
+    if ($depth > 3) {
+        return;
+    }
+
+    $jsFile = $bundle['file'];
+    $cssFiles = isset($bundle['css']) ? $bundle['css'] : [];
+    $scriptTag = $depth === 0 ? 'the-countdown-timer' : basename($jsFile);
+    $scriptPath = THE_CDT_PLUGIN_COMPONENTS_BUILD_URL . '/' . $jsFile;
+    $scriptVersion = file_exists($scriptPath) ? filemtime($scriptPath) : null;
+
+    if (wp_script_is($scriptTag, 'enqueued')) {
+        return;
+    }
+
+    if (function_exists('wp_register_script_module')) { // wp_register_script_module & wp_enqueue_script_module (WP6.5+)
+        wp_register_script_module($scriptTag, $scriptPath, ['wp-element'], $scriptVersion, true);
+        wp_enqueue_script_module($scriptTag);
+    } else {
+        wp_register_script($scriptTag, $scriptPath, ['wp-element'], $scriptVersion, true);
+        wp_enqueue_script($scriptTag);
+        add_filter('script_loader_tag', function ($tag, $handle, $src) use ($scriptTag) {
+            if ($handle === $scriptTag) {
+                return str_replace('<script ', '<script type="module" ', $tag);
+            }
+            return $tag;
+        }, 10, 3);
+    }
+
+    foreach ($cssFiles as $cssFile) {
+        $cssPath = THE_CDT_PLUGIN_COMPONENTS_BUILD_URL . '/' . $cssFile;
+        $cssVersion = file_exists($cssPath) ? filemtime($cssPath) : null;
+
+        wp_enqueue_style(
+            basename($cssFile),
+            $cssPath,
+            [],
+            $cssVersion
+        );
+    }
+
+    if (isset($bundle['imports'])) {
+        foreach ($bundle['imports'] as $import) {
+            if (isset($manifest[$import])) {
+                the_cdt_enqueue_bundle($manifest, $manifest[$import], ++$depth);
+            }
+        }
     }
 }
